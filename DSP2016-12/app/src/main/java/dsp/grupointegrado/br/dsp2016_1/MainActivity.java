3e579ec042c28ffa2c150294b46c9951faa18347
Bruno Pachulski <yakko.acme@gmail.com>
2016-02-17 21:11:02 -0200

    Final da aula de 17/02/2016

--- DSP2016-12/app/src/main/java/dsp/grupointegrado/br/dsp2016_1/MainActivity.java
+++ DSP2016-12/app/src/main/java/dsp/grupointegrado/br/dsp2016_1/MainActivity.java
@@ -2,12 +2,31 @@ package dsp.grupointegrado.br.dsp2016_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+
+
+        final Button btn = (Button) findViewById(R.id.btnAula1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getBaseContext(), "Oiiii " + btn.getText().toString(), Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
+
 }
