35b65c67d4d06e3ef26edd4269116b8cd8e45c7a
Bruno Pachulski <yakko.acme@gmail.com>
2016-04-20 21:10:09 -0300

    Projeto com AA

---
+++ DSP201612bim/app/src/main/java/dsp/grupointegrado/br/dsp201612bim/MainActivity.java
@@ -0,0 +1,12 @@
+package dsp.grupointegrado.br.dsp201612bim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+
+}
